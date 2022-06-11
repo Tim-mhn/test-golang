c2b35dd10df6f6b0f8e7e14b8d2dff75cf3ce015
Timothy Meehan <tim.meehan097@gmail.com>
2022-06-11 10:35:55 +0700

    worked with structs and import/exports

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/tim-mhn/module-1/models"
+)
 
 const (
 	first = iota
@@ -36,10 +40,79 @@ func main() {
 	// fmt.Println(*ptr)
 
 	// Constant declarations
-	const i int = 3
-	fmt.Println(i)
-	const j = float32(i) + 2.21312
-	fmt.Println((j))
+	// const i int = 3
+	// fmt.Println(i)
+	// const j = float32(i) + 2.21312
+	// fmt.Println((j))
 
-	fmt.Println(first, two)
+	// fmt.Println(first, two)
+
+	// Arrays
+	// var arr [3]int = [3]int{1, 2, 3}
+	// arr := [3]int{1, 2, 3}
+	// slice := arr[:]
+
+	// arr[1] = 10
+	// // arr = [1, 2 ,1]
+	// slice[0] = 26 // all the elements have the same ref as those in arr
+
+	// slice2 := []int{1, 2, 10}
+	// slice3 := append(slice2, 4) // copies all elements, not same ref anymore
+
+	// s4 := slice[1:]
+	// s5 := slice3[2:3]
+
+	// slice2[0] = 5
+	// fmt.Println(arr)
+	// fmt.Println(slice)
+	// fmt.Println(slice2)
+	// fmt.Println(slice3)
+	// fmt.Println(s4)
+	// fmt.Println(s5)
+
+	// Maps
+	// m := map[string]int{"foo": 4}
+
+	// fmt.Println(m)
+
+	// m["foo"] = 10
+
+	// fmt.Println(m)
+
+	// m["bar"] = 5
+
+	// fmt.Println(m)
+
+	// delete(m, "foo")
+
+	// fmt.Println(m)
+
+	// Structs
+
+	// type user struct {
+	// 	name    string
+	// 	surname string
+	// 	age     int
+	// }
+
+	// var u user
+	// u.name = "tim"
+	// u.age = 24
+	// u.surname = "mhn"
+
+	// u := user{age: 24, name: "tim", surname: "mhn"}
+
+	// var u2 user = user{age: 25, name: "tim2", surname: "mhn"}
+
+	// fmt.Println(u)
+	// fmt.Println(u2)
+
+	u := models.User{
+		Id:        1,
+		FirstName: "tim",
+		LastName:  "mhn",
+	}
+
+	fmt.Println(u)
+
 }
